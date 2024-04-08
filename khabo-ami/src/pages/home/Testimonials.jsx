2a655ac0548f449e48e5abbfe0596bf88f185642
Akib-Raihan-4 <raihanakib062@gmail.com>
2024-04-08 14:10:43 +0600

    Testimonials section

---
+++ khabo-ami/src/pages/home/Testimonials.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div className="section-container">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-12">
+        <div className="md:w-1/2">
+          <img src="/home/testimonials/testimonials.png" alt="" />
+        </div>
+        <div className="md:w-1/2">
+          <div className="text-left md:h-4/5">
+            <p className="subtitle">Testimonials</p>
+            <h2 className="title">What Our Customers Say About Us</h2>
+            <blockquote className="my-5 text-secondary leading-[30px]">
+              “I had the pleasure of dining at Foodi last night, and I'm still
+              raving about the experience! The attention to detail in
+              presentation and service was impeccable”
+            </blockquote>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
