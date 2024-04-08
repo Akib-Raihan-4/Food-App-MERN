f01664f51d10e134cb9e38c24b3a5b18ebe33fa3
Akib-Raihan-4 <raihanakib062@gmail.com>
2024-04-08 14:30:46 +0600

    Testimonial Section done

--- khabo-ami/src/pages/home/Testimonials.jsx
+++ khabo-ami/src/pages/home/Testimonials.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaStar } from "react-icons/fa";
+
 
 const Testimonials = () => {
   return (
@@ -16,10 +18,32 @@ const Testimonials = () => {
               raving about the experience! The attention to detail in
               presentation and service was impeccable”
             </blockquote>
-            <div>
-                <div>
-                    
+            <div className="flex items-center gap-4 flex-wrap">
+              <div className="avatar-group -space-x-6 rtl:space-x-reverse">
+                <div className="avatar">
+                  <div className="w-12">
+                    <img src="/home/testimonials/testimonial2.png" />
+                  </div>
                 </div>
+                <div className="avatar">
+                  <div className="w-12">
+                    <img src="/home/testimonials/testimonial2.png" />
+                  </div>
+                </div>
+                <div className="avatar">
+                  <div className="w-12">
+                    <img src="/home/testimonials/testimonial2.png" />
+                  </div>
+                </div>
+              </div>
+              <div className="space-y-1">
+                <h5 className="text-lg font-semibold">Customer Feedback</h5>
+                <div className="flex items-center gap-2">
+                  <FaStar className="text-yellow-400" />{" "}
+                  <span className="font-medium">4.9</span>{" "}
+                  <span className="text-[#807E7E]">(18.6k Reviews)</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
